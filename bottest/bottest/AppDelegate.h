e27861efd47b3cea434e46a814295035dd832804
guanghshuai.li <guangshuai.li@innocellence.com>
2015-08-14 09:56:34 +0800

    ios

---
+++ bottest/bottest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  bottest
+//
+//  Created by Breeze on 15/8/14.
+//  Copyright (c) 2015年 Breeze. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
